2769b96c44b48dbbd5bee3091c7219e80b780e0d
Slyne Deng <slynedeng@gmail.com>
2020-10-07 14:07:37 +0800

    Update DaCiDian.py
    adapted to python3

--- DaCiDian.py
+++ DaCiDian.py
@@ -13,15 +13,16 @@ syllable_to_phones={}
 
 word_to_syllable_file  = sys.argv[1]  # layer-1 mapping
 syllable_to_phone_file = sys.argv[2]  # layer-2 mapping
-
-for l in open(syllable_to_phone_file):  # "ZHENG	zh eng"
+out = sys.argv[3]
+out = open(out, "w", encoding="utf-8")
+for l in open(syllable_to_phone_file, "r", encoding="utf-8"):  # "ZHENG	zh eng"
 	cols = l.strip().split('\t')
 	assert(len(cols) == 2)
 	syllable = cols[0]
 	phones   = cols[1].split()
 	syllable_to_phones[syllable] = phones
 
-for l in open(word_to_syllable_file): # "15	YI_1 WU_3;YAO_1 WU_3"
+for l in open(word_to_syllable_file, "r", encoding="utf-8"): # "15	YI_1 WU_3;YAO_1 WU_3"
 	cols = l.strip().split('\t')
 	assert(len(cols) == 2)
 	word  = cols[0]
@@ -33,4 +34,4 @@ for l in open(word_to_syllable_file): # "15	YI_1 WU_3;YAO_1 WU_3"
 			phones = [phn for phn in syllable_to_phones[base]]
 			phones[-1] = phones[-1]+'_'+tone
 			phone_seq.extend(phones)
-		sys.stdout.write(word + '\t' + ' '.join(phone_seq) + '\n')
+		out.write(word + '\t' + ' '.join(phone_seq) + '\n')
